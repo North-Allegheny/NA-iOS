fd6260ec0e9a0b616dd8e3157d265312cd8df5d7
alex-taffe <alex.taffe@gmail.com>
2015-01-12 15:12:06 -0500

    First
    First

---
+++ NA/NA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NA
+//
+//  Created by Taffe, Alexander on 1/12/15.
+//  Copyright (c) 2015 NA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
