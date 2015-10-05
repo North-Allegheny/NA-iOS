9345aa067c9cd96babb244b423271bbdcc8de108
Alex Taffe <alex.taffe@gmail.com>
2015-10-05 07:33:50 -0400

    Add new app project

---
+++ NA/NA/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NA
+//
+//  Created by NA App on 10/5/15.
+//  Copyright © 2015 NA App. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
