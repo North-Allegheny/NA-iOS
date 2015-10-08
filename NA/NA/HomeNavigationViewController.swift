a94b2cf9fd4181c4278d3c80e81e15e13c571a16
Alex Taffe <alex.taffe@gmail.com>
2015-10-08 08:20:20 -0400

    Add menu to storyboard and create debug segue

---
+++ NA/NA/HomeNavigationViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeNavigationViewController.swift
+//  NA
+//
+//  Created by NA App on 10/8/15.
+//  Copyright © 2015 NA App. All rights reserved.
+//
+
+import UIKit
+
+class HomeNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
