f1b54c9d69ddccd3590ea007b22c6bf0a10678c8
Alex Taffe <alex.taffe@gmail.com>
2015-10-23 17:21:18 -0400

    Add blur library

--- NA/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ NA/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -86,6 +86,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/OnePasswordExtension.framework"
   install_framework "Pods/AFNetworking.framework"
+  install_framework "Pods/FXBlurView.framework"
   install_framework "Pods/Realm.framework"
   install_framework "Pods/RealmSwift.framework"
   install_framework "Pods/SSKeychain.framework"
@@ -93,6 +94,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/OnePasswordExtension.framework"
   install_framework "Pods/AFNetworking.framework"
+  install_framework "Pods/FXBlurView.framework"
   install_framework "Pods/Realm.framework"
   install_framework "Pods/RealmSwift.framework"
   install_framework "Pods/SSKeychain.framework"
