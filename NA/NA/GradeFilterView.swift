202aa5210516e8195b676b2f1475d3f97bf9595d
Alex Taffe <alex.taffe@gmail.com>
2016-03-17 15:40:55 -0400

    Begin adding horizontal scroll

---
+++ NA/NA/GradeFilterView.swift
@@ -0,0 +1,32 @@
+//
+//  GradeFilterView.swift
+//  NA
+//
+//  Created by Alex on 3/17/16.
+//  Copyright © 2016 NA App. All rights reserved.
+//
+
+import UIKit
+import AKPickerView_Swift
+
+class GradeFilterView: UIView {
+    
+    var picker
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
